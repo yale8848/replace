3cc08d0eef5c4d15eddaed3cd6d610951b184f44
yale8848 <royal8848@163.com>
2018-03-09 11:44:51 +0800

    add gofmt

--- main/replace.go
+++ main/replace.go
@@ -2,43 +2,43 @@
 package main
 
 import (
-	"flag"
-	"io/ioutil"
-	"fmt"
 	"encoding/json"
-	"replace/rconfig"
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"os"
-	"regexp"
 	"path/filepath"
+	"regexp"
+	"replace/rconfig"
 )
 
-func checkError(err error)  {
-	if err!=nil {
+func checkError(err error) {
+	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 }
 
-func getFiles(files []string,file string,recursive bool) []string{
+func getFiles(files []string, file string, recursive bool) []string {
 
-	f,err:=os.Open(file)
+	f, err := os.Open(file)
 	checkError(err)
-	if s,err:=f.Stat();err==nil {
+	if s, err := f.Stat(); err == nil {
 		if s.IsDir() {
-			fi,err:=ioutil.ReadDir(file)
+			fi, err := ioutil.ReadDir(file)
 			checkError(err)
-			for _,f:=range fi  {
+			for _, f := range fi {
 				if f.IsDir() {
 					if recursive {
-						return getFiles(files,filepath.Join(file,f.Name()),recursive)
+						return getFiles(files, filepath.Join(file, f.Name()), recursive)
 					}
-				}else{
-					files  = append(files,filepath.Join(file,f.Name()))
+				} else {
+					files = append(files, filepath.Join(file, f.Name()))
 				}
 			}
 
-		}else{
-			files  = append(files,file)
+		} else {
+			files = append(files, file)
 		}
 	}
 	f.Close()
@@ -47,50 +47,49 @@ func getFiles(files []string,file string,recursive bool) []string{
 
 }
 
-func main()  {
+func main() {
 
-	config:=flag.String("c","config.json","config.json")
+	config := flag.String("c", "config.json", "config.json")
 
 	flag.Parse()
 
-	dat,err:=ioutil.ReadFile(*config)
+	dat, err := ioutil.ReadFile(*config)
 	checkError(err)
 
-	rc:=&rconfig.RConfig{}
-	err=json.Unmarshal(dat,&rc)
+	rc := &rconfig.RConfig{}
+	err = json.Unmarshal(dat, &rc)
 	checkError(err)
 
-	for _,c:=range rc.Replace  {
+	for _, c := range rc.Replace {
 
-		for _,p:=range c.Paths{
+		for _, p := range c.Paths {
 
-			f,err:=os.Open(p)
+			f, err := os.Open(p)
 			checkError(err)
-            files:=make([]string,0)
+			files := make([]string, 0)
 
-			files = getFiles(files,p,c.Recursive)
+			files = getFiles(files, p, c.Recursive)
 
-			for _,f:=range files  {
-				dat,err:=ioutil.ReadFile(f)
+			for _, f := range files {
+				dat, err := ioutil.ReadFile(f)
 				checkError(err)
 				data := string(dat)
 
-				rp:=data
-				for _,rpls:=range c.RegReplace  {
-					m,_:=regexp.MatchString(rpls.Regex,data)
+				rp := data
+				for _, rpls := range c.RegReplace {
+					m, _ := regexp.MatchString(rpls.Regex, data)
 					if !m {
 						continue
 					}
 					r, _ := regexp.Compile(rpls.Regex)
-					rp=r.ReplaceAllString(rp,rpls.Replacement)
+					rp = r.ReplaceAllString(rp, rpls.Replacement)
 					if !c.Silent {
-						fmt.Printf("[replacing]:%v: %v-->%v\r\n",f,rpls.Regex,rpls.Replacement)
+						fmt.Printf("[replacing]:%v: %v-->%v\r\n", f, rpls.Regex, rpls.Replacement)
 					}
 				}
-
-				if rp!=data {
-					fmt.Println("[replaced]:"+f)
-					err=ioutil.WriteFile(f,[]byte(rp),0666)
+				if rp != data {
+					fmt.Println("[replaced]:" + f)
+					err = ioutil.WriteFile(f, []byte(rp), 0666)
 					checkError(err)
 				}
 			}
